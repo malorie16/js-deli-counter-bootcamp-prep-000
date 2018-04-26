7155393617b87448fa3597189da1614fe21d9da4
malorie16 <malorie18@gmail.com>
2018-04-26 13:25:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,31 +1,30 @@
-var katzDeliLine = []
 
 function takeANumber(katzDeliLine, name) {
-    katzDeliLine.push(name)
+  katzDeliLine.push(name)
   return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
 }
 
 function nowServing(katzDeliLine) {
-   if (katzDeliLine.length == 0) {
-     return("There is nobody waiting to be served!")
+  if (katzDeliLine.length === 0) {
+    return "There is nobody waiting to be served!"
   }
-   else {
-     return(`Currently serving ${katzDeliLine.shift()}.`)
-   }
+  else {
+    return `Currently serving ${katzDeliLine.shift()}.`
+  }
 }
 
 function currentLine(katzDeliLine) {
-  if (katzDeliLine == 0) {
+  if (katzDeliLine.length === 0) {
     return "The line is currently empty."
   }
   else {
-    return "The line is currently:" + katzDeliLine.map(function(name, pos) {
-      if  (katzDeliLine[0]) {
-        return ` ${(pos + 1)}. ${name}`
+    return "The line is currently: " + katzDeliLine.map(function(name, position) {
+      if (position === 0) {
+        return `${(position + 1)}. ${name}`
       }
       else {
-      return  ` ${(pos + 1)}. ${name}`
+      return ` ${(position + 1)}. ${name}`
     }
     })
-    }
   }
+}
